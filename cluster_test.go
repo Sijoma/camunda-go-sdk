a6deb61bc665ade1f2511672ae0181f8fb8a512d
Simon Zengerling <simon.zengerling@camunda.com>
2025-06-07 13:19:58 +0200

    fix: rename module for now

--- cluster_test.go
+++ cluster_test.go
@@ -8,8 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/camunda/go-sdk"
-	"github.com/camunda/go-sdk/internal"
+	"github.com/sijoma/camunda-go-sdk"
+	"github.com/sijoma/camunda-go-sdk/internal"
 )
 
 func TestWithCamunda(t *testing.T) {
