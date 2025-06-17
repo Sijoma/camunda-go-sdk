d30892a71534a90f7b07e43cbf7779c44d2df28c
Simon Zengerling <simon.zengerling@camunda.com>
2025-06-17 21:07:24 +0200

    feat(internal): health check test suite

--- internal/test_suite.go
+++ internal/test_suite.go
@@ -3,11 +3,15 @@ package internal
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"testing"
 	"time"
 
+	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	"github.com/sijoma/camunda-go-sdk"
 )
 
 type TestSuite struct {
@@ -44,10 +48,12 @@ func (ts *TestSuite) setupCamunda(ctx context.Context) error {
 		Image:        "camunda/zeebe:latest",
 		ExposedPorts: []string{"26500/tcp", "8080/tcp"},
 		WaitingFor: wait.ForAll(
+			&topologyWaitStrategy{},
 			wait.ForLog("Tomcat started on port 8080 (http) with context path"),
 			wait.ForLog("Tomcat started on port 9600"),
 		).WithDeadline(2 * time.Minute),
 	}
+
 	camundaContainer, err := testcontainers.GenericContainer(
 		ctx,
 		testcontainers.GenericContainerRequest{
@@ -72,4 +78,57 @@ func (ts *TestSuite) teardown(ctx context.Context) {
 			ts.t.Errorf("failed to terminate container: %v", err)
 		}
 	}
+}
+
+type topologyWaitStrategy struct{}
+
+func (t topologyWaitStrategy) WaitUntilReady(ctx context.Context, target wait.StrategyTarget) error {
+	maxAttempts := 30
+	backoff := 2 * time.Second
+
+	for attempt := 1; attempt <= maxAttempts; attempt++ {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("context cancelled while waiting for topology: %w", ctx.Err())
+		default:
+			host, err := target.Host(ctx)
+			if err != nil {
+				return err
+			}
+
+			zeebePort, err := nat.NewPort("", "8080")
+			if err != nil {
+				return err
+			}
+
+			mappedPort, err := target.MappedPort(ctx, zeebePort)
+			if err != nil {
+				return err
+			}
+
+			baseURL, err := url.Parse(fmt.Sprintf("http://%s:%d", host, mappedPort.Int()))
+			if err != nil {
+				return err
+			}
+
+			client, err := camunda.NewClient(camunda.WithBaseURL(*baseURL))
+			if err != nil {
+				return err
+			}
+
+			topology, err := client.Cluster.Topology(ctx)
+			if err != nil {
+				fmt.Printf("TestSetup HealthCheck: Attempt %d/%d: Failed to get topology: %v\n", attempt, maxAttempts, err)
+			} else if topology.ClusterSize > 0 && len(topology.Brokers) > 0 {
+				return nil // Success!
+			}
+
+			if attempt < maxAttempts {
+				time.Sleep(backoff)
+			}
+		}
+	}
+
+	return fmt.Errorf("timeout waiting for cluster topology after %d attempts", maxAttempts)
+
 }
