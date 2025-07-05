ab224eb53e92c5b28d48b4031797b16656dd37df
hamza-m-masood <hamzamasood183@gmail.com>
2025-07-05 23:40:27 +0100

    deploy-resource

--- orchestration/resource.go
+++ orchestration/resource.go
@@ -5,12 +5,85 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"path"
 )
 
 type Resource struct {
 	client *Client
+}
+
+// TODO: change Resources from string to binary
+type ResourceDeployRequest struct {
+	Resources string `json:"resources"`
+	TenantId  string `json:"tenantId"`
+}
+
+// TODO: change Deployments from string to binary
+type ResourceDeployResponse struct {
+	TenantId      string `json:"resources"`
+	DeploymentKey string `json:"tenantId"`
+	Deployments   string `json:"deployments"`
+}
+
+func (r Resource) Deploy(ctx context.Context, request ResourceDeployRequest, filePath string) (*ResourceDeployResponse, error) {
+	u := r.client.baseURL
+	u.Path = path.Join(r.client.baseURL.Path, "deployments")
+
+	buf := &bytes.Buffer{}
+	mpw := multipart.NewWriter(buf)
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	fWriter, err := mpw.CreateFormFile("resources", filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = io.Copy(fWriter, f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Close the multipart writer before creating the request
+	err = mpw.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	// set up the request
+
+	req, err := http.NewRequestWithContext(ctx, "POST", u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Content-Type", mpw.FormDataContentType()) // detect the form data content type
+
+	resp, err := r.client.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(u.String())
+	fmt.Println(body)
+	fmt.Println(resp.StatusCode)
+
+	defer f.Close()
+	defer resp.Body.Close()
+
+	fmt.Println(u.Path)
+	return nil, nil
 }
 
 type ResourceGetRequest struct {
